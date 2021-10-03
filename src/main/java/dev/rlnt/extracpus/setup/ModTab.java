b92d50ca119a16b170481055b366cd039007e103
Relentless <relentless@rlnt.dev>
2021-10-03 03:34:41 +0200

    added dependency warnings

--- src/main/java/dev/rlnt/extracpus/setup/ModTab.java
+++ src/main/java/dev/rlnt/extracpus/setup/ModTab.java
@@ -2,7 +2,11 @@ package dev.rlnt.extracpus.setup;
 
 import static dev.rlnt.extracpus.Constants.MOD_ID;
 
+import dev.rlnt.extracpus.ExtraCPUs;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -16,6 +20,10 @@ public class ModTab extends CreativeTabs {
     @SideOnly(Side.CLIENT)
     @Override
     public ItemStack createIcon() {
-        return new ItemStack(ModBlocks.CRAFTING_STORAGE_16384K.maybeItem().orElseThrow(NullPointerException::new));
+        return new ItemStack(
+            ExtraCPUs.EXISTING_DEPENDENCY
+                ? ModBlocks.CRAFTING_STORAGE_16384K.maybeItem().orElse(Items.POTATO)
+                : Item.getItemFromBlock(Blocks.BARRIER)
+        );
     }
 }
